1815f10a7dc56f8848fabd500fd936b18fbb8604
Dustin Machi <dmachi@vbi.vt.edu>
2016-10-16 12:10:36 -0400

    some jsonrpc updates

--- media/json.js
+++ media/json.js
@@ -3,14 +3,14 @@ var addMedia = require("../media").addMedia;
 addMedia({
 	"content-type": "application/json",
 	serialize: function(results,options){
-		return JSON.stringify(results.results);
+		return JSON.stringify(results.results || results);
 	}
 })
 
 addMedia({
 	"content-type": "text/json",
 	serialize: function(results,options){
-		return JSON.stringify(results);
+		return JSON.stringify(results||results);
 	}
 })
 
